70244b7cebf736744422f9daf5cec272ff2040d9
mm1995tk <jimi.woodstock1995@gmail.com>
2024-05-06 16:56:17 +0900

    微修正

--- webapi/src/framework/system.rs
+++ webapi/src/framework/system.rs
@@ -4,12 +4,12 @@ use super::logger::{Logger, LoggerInterface};
 use axum::response::{IntoResponse, Response};
 use reqwest::StatusCode;
 
-pub struct Panic<T: Into<String>>(pub T);
+pub struct Panic(pub String);
 
 #[derive(Debug)]
-pub enum AppError<'a> {
+pub enum AppError {
     /// 業務上xxxなはずだからunwrapする時に使う
-    Unexpected(&'a Logger, String),
+    Unexpected(Logger, String),
     AuthenticationError,
     AutorizationError(String),
     /// ワークフローの最中に発生したエラー
@@ -17,25 +17,27 @@ pub enum AppError<'a> {
 }
 
 pub trait IntoAppError: Sized {
-    fn into_app_error(self, l: &Logger) -> AppError;
+    fn into_app_error(self, l: Logger) -> AppError;
 }
 
-impl<T: Into<String>> IntoAppError for Panic<T> {
-    fn into_app_error(self, l: &Logger) -> AppError {
-        AppError::Unexpected(l, self.0.into())
+impl IntoAppError for Panic {
+    fn into_app_error(self, l: Logger) -> AppError {
+        AppError::Unexpected(l, self.0)
     }
 }
 
-pub trait DomainError: Debug + Error + IntoAppError {
-    fn from_panic<T: Into<String>>(e: Panic<T>) -> Self
+pub trait DomainError: IntoAppError {
+    fn from_panic(e: Panic) -> Self
     where
         Self: Sized;
 }
 
-impl<'a> IntoResponse for AppError<'a> {
+impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         match self {
-            AppError::AuthenticationError => (StatusCode::UNAUTHORIZED, "認証エラー").into_response(),
+            AppError::AuthenticationError => {
+                (StatusCode::UNAUTHORIZED, "認証エラー").into_response()
+            }
             AppError::AutorizationError(msg) => (StatusCode::FORBIDDEN, msg).into_response(),
             AppError::WorkflowException(code, msg) => (code, msg).into_response(),
 
